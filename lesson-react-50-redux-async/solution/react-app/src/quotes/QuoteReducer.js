30f806ad86df141e68410caa1f3b44b4fe1c7c62
Clay Boyd <clboyd@paypal.com>
2017-09-19 13:28:23 -0500

    Added redux thunk to the course

--- lesson-react-50-redux-async/solution/react-app/src/quotes/QuoteReducer.js
+++ lesson-react-50-redux-async/solution/react-app/src/quotes/QuoteReducer.js
@@ -3,22 +3,20 @@ import { QUOTE_REQUEST, QUOTE_RECEIVE } from './QuoteConstants'
 // TODO add the isDelayed property to the initialState, set it to false
 const initialState = { name: '', quote: '', isDelayed: false };
 
-export default function nameReducer(state = initialState, action) {
-
+export default function quoteReducer(state = initialState, action) {
 	switch (action.type) {
-
 		case QUOTE_RECEIVE:
 			return {
 				...state,
-				...action.payload
+				name: action.payload.name,
+				quote: action.payload.quote,
+				isDelayed: action.payload.isDelayed,
 			};
-
 		case QUOTE_REQUEST:
 			return {
 				...state,
-				...action.payload
+				isDelayed: action.payload.isDelayed,
 			};
-
 		default:
 			return state
 	}
