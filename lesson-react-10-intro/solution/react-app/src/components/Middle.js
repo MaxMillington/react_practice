3f55f07fcf6cd0e710baec84dec029348a1d72bc
Clay Boyd <clboyd@paypal.com>
2017-10-05 13:17:04 -0500

    There is no lab for the first section, so no solutions folder

--- lesson-react-10-intro/solution/react-app/src/components/Middle.js
+++
@@ -1,9 +0,0 @@
-import React, {Component} from 'react'
-
-export default class Middle extends Component {
-  render() {
-    return (
-      <div className="middle">{this.props.text}</div>
-    )
-  }
-}
