1ce370526e2978e6f8e542a7801f70a923c32136
Clay Boyd <clboyd@paypal.com>
2017-10-19 17:29:09 -0500

    The lab had solution code in it.  Reverted to original lab

--- lesson-react-32-shared-state/lab/shared/src/components/UserInfo.js
+++ lesson-react-32-shared-state/lab/shared/src/components/UserInfo.js
@@ -9,23 +9,8 @@ export function getName() {
 export function setName(name) {
   console.log('name is ' + name)
   userName = name;
-  emitEvent( name )
 }
 
 export default {
-  getName, setName, registerListener, unregisterListener
-}
-
-let listeners = []
-
-export function registerListener( callback ) {
-  listeners.push( callback )
-}
-
-export function unregisterListener( callback ) {
-  listeners = listeners.filter( cb => cb !== callback )
-}
-
-function emitEvent( name ) {
-  listeners.forEach( (cb) => cb( name ))
+  getName, setName
 }
